08c0146eb759318eab414965a0aec841fb0a1fe3
JAKUAN-AHMED <jakuanultimate777@gmail.com>
2024-05-29 17:42:54 +0600

    finally i have finished my project properly

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,15 +1,16 @@
+import bannerImg from '../../assets/images/Proffesional-removebg-preview.png';
 const Banner = () => {
     return (
       <div className="hero min-h-screen bg-base-200 border rounded-xl">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <img
-            src="/src/assets/images//Proffesional-removebg-preview.png"
+            src={bannerImg}
             className="max-w-lg rounded-lg border border-blue-400"
           />
           <div>
             <div className="pl-16">
               <h1 className="text-5xl font-extrabold text-start">
-                One Step <br /> Closer To Your <br />{" "}
+                One Step <br /> Closer To Your <br />
                 <span className="text-[#7E90FE]">Dream Job</span>
               </h1>
             </div>
--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,10 +1,10 @@
 const Category = ({category}) => {
-    const {logo,category_name,availability,id}=category;
+    const {logo,availability,category_name}=category;
     return (
       <div className="card card-compact bg-base-100 shadow-xl text-start">
         <div className="bg-purple-100 w-20 h-20 border rounded shadow-lg m-4">
           <figure className="p-6">
-            <img src={logo} alt="Shoes" />
+            <img src={logo} alt="logo" />
           </figure>
         </div>
         <div className="card-body">
